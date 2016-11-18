a5573d65f794b1bef6e2906ee74f6559e21a35da
eXeCUT <sexecut@gmail.com>
2016-11-18 18:10:11 +0300

    Added full export option

--- action/adapter/viewRenderer/DynaGrid.php
+++ action/adapter/viewRenderer/DynaGrid.php
@@ -9,6 +9,7 @@ namespace execut\actions\action\adapter\viewRenderer;
 
 
 use execut\yii\helpers\Html;
+use kartik\export\ExportMenu;
 use yii\bootstrap\Alert;
 use yii\helpers\Url;
 
@@ -49,13 +50,44 @@ class DynaGrid extends Widget
             }
         }
 
+        $columns = $this->filter->getGridColumns();
 //        $flash = '<aasd';
         $alertBlock = $this->renderAlertBlock();
+        $fullExportMenu = ExportMenu::widget([
+            'dataProvider' => $this->dataProvider,
+//            'dataProvider' => $dataProvider,
+            'columns' => $columns,
+            'target' => ExportMenu::TARGET_BLANK,
+            'batchSize' => 1000,
+            'fontAwesome' => true,
+            'asDropdown' => false, // this is important for this case so we just need to get a HTML list
+            'dropdownOptions' => [
+                'label' => '<i class="glyphicon glyphicon-export"></i> Full'
+            ],
+        ]);
 
         return [
             'class' => \kartik\dynagrid\DynaGrid::className(),
             'storage' => \kartik\dynagrid\DynaGrid::TYPE_SESSION,
+//            'pageSize' => 100000,
             'gridOptions' => [
+                'export' => [
+                    'fontAwesome' => true,
+                    'itemsAfter'=> [
+                        '<li role="presentation" class="divider"></li>',
+                        '<li class="dropdown-header">Export All Data</li>',
+                        $fullExportMenu
+                    ]
+                ],
+                'toggleDataOptions' => [
+                    'maxCount' => 100000,
+//                    'all' => [
+//                       'icon' => 'resize-full',
+//                       'label' => 'All',
+//                       'class' => 'btn btn-default',
+//                       'title' => 'Show all data'
+//                    ],
+                ],
                 'filterModel' => $this->filter,
                 'afterHeader' => $alertBlock,
                 'toolbar' => [
@@ -63,6 +95,7 @@ class DynaGrid extends Widget
                         Html::a('<i class="glyphicon glyphicon-repeat"></i>', $refreshUrlParams, ['data-pjax' => 0, 'class' => 'btn btn-default', 'title' => 'Reset Grid'])
                     ],
                     ['content' => '{dynagridFilter}{dynagridSort}{dynagrid}'],
+                    '{toggleData}',
                     '{export}',
                 ],
                 'panel' => [
@@ -73,7 +106,7 @@ class DynaGrid extends Widget
             'options' => [
                 'id' => $modelClass::getModelId(),
             ],
-            'columns' => $this->filter->getGridColumns(),
+            'columns' => $columns,
         ];
     }
 
