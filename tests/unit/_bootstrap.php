66e56bfc2a2269e11025e5d6b97f5d660eacdfc6
eXeCUT <sexecut@gmail.com>
2017-06-23 17:18:00 +0300

    Added scrutinizer

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -10,9 +10,14 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 defined('YII_ENV') or define('YII_ENV', 'test');
 
-require_once(__DIR__ . '/../../vendor/autoload.php');
+if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    require_once(__DIR__ . '/../../vendor/autoload.php');
 
-require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+    require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+} else {
+    require_once(__DIR__ . '/../../../../autoload.php');
+    require_once(__DIR__ . '/../../../../yiisoft/yii2/Yii.php');
+}
 
 //require(__DIR__ . '/../../common/config/aliases.php');
 
