75e2382705732c32f296c60d18356a00c3742548
eXeCUT <sexecut@gmail.com>
2017-06-23 16:01:18 +0300

    fix

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -10,6 +10,10 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 defined('YII_ENV') or define('YII_ENV', 'test');
 
+require_once('autoload.php');
+
+require_once('yiisoft/yii2/Yii.php');
+
 //require(__DIR__ . '/../../common/config/aliases.php');
 
 // set correct script paths
