49cb1c34de60440c6804bfed1c40a5ddf74db9ca
eXeCUT <sexecut@gmail.com>
2018-03-16 17:10:14 +0300

    Added ability for grid rows selection

--- widgets/assets/GridView.js
+++ widgets/assets/GridView.js
@@ -1,5 +1,6 @@
 (function () {
     $.widget("execut.GridView", {
+        _moveStarted: false,
         _create: function () {
             var t = this;
             t._initElements();
@@ -9,9 +10,19 @@
             var t = this,
                 el = t.element;
             if (typeof t.options.updateUrl !== 'undefined') {
+                el.find('tr[data-key]').mousemove(function (e) {
+                    t._moveStarted = true;
+                });
+
+                el.find('tr[data-key]').mousedown(function (e) {
+                    t._moveStarted = false;
+                });
                 el.find('tr[data-key]').click(function (e) {
                     var trEl = $(this),
                         targetEl = $(e.target);
+                    if (t._moveStarted) {
+                        return false;
+                    }
                     if (!(targetEl.parents('a').length || targetEl.is('a'))) {
                         var id = trEl.attr('data-id'),
                             url = t.options.updateUrl,
