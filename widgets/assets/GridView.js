2ca6d8b1d906f4ab5166b55476a5d69772078683
eXeCUT <sexecut@gmail.com>
2018-03-29 17:48:38 +0300

    Added links emulation in grid

--- widgets/assets/GridView.js
+++ widgets/assets/GridView.js
@@ -10,32 +10,61 @@
             var t = this,
                 el = t.element;
             if (typeof t.options.updateUrl !== 'undefined') {
-                el.find('tr[data-key]').mousemove(function (e) {
+                el.find('tr[data-id]').mousemove(function (e) {
                     t._moveStarted = true;
                 });
 
-                el.find('tr[data-key]').mousedown(function (e) {
-                    t._moveStarted = false;
+                el.find('tr[data-id]').mousedown(function (e) {
+                    if (e.originalEvent.button == 0) {
+                        t._moveStarted = false;
+                    } else {
+                        var trEl = $(this),
+                            href = false;
+                        if (href = t._getHrefFromTr(trEl, e)) {
+                            var tdEl = $(e.target).css('position', 'relative'),
+                                linkEl = $('<a href="' + href + '" style="width: 100%;left: 0; top: 0; height: 100%; display: block; position: absolute"></a>');
+
+                            tdEl.append(linkEl);
+                            setTimeout(function () {
+                                linkEl.remove();
+                            }, 10);
+                        }
+
+                        return false;
+                    }
                 });
-                el.find('tr[data-key]').click(function (e) {
-                    var trEl = $(this),
-                        targetEl = $(e.target);
+
+                el.find('tr[data-id]').click(function (e) {
                     if (t._moveStarted) {
                         return false;
                     }
-                    if (!(targetEl.parents('a').length || targetEl.is('a'))) {
-                        var id = trEl.attr('data-id'),
-                            url = t.options.updateUrl,
-                            delimiter = null;
-                        if (url.search('\\?') !== -1) {
-                            delimiter = '&';
-                        } else {
-                            delimiter = '?';
-                        }
 
-                        location.href = url + delimiter + 'id=' + id;
+                    var trEl = $(this),
+                        href = false;
+                    if (href = t._getHrefFromTr(trEl, e)) {
+                        if (e.originalEvent.ctrlKey) {
+                            window.open(href, '_blank');
+                        } else {
+                            location.href = href;
+                        }
                     }
                 });
+            }
+        },
+        _getHrefFromTr: function (trEl, e) {
+            var t = this,
+                targetEl = $(e.target);
+            if (!(targetEl.parents('a').length || targetEl.is('a'))) {
+                var id = trEl.attr('data-id'),
+                    url = t.options.updateUrl,
+                    delimiter = null;
+                if (url.search('\\?') !== -1) {
+                    delimiter = '&';
+                } else {
+                    delimiter = '?';
+                }
+
+                return url + delimiter + 'id=' + id;
             }
         },
         _initEvents: function () {
