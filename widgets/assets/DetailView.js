6db4ebb02142c98ca917ae1d14769c0bdef65736
eXeCUT notebook <sexecut@gmail.com>
2019-12-26 15:34:46 +0300

    Added floated button

--- widgets/assets/DetailView.js
+++ widgets/assets/DetailView.js
@@ -1,5 +1,7 @@
 (function () {
     $.widget("execut.DetailView", {
+        buttonsEl: null,
+        topOffset: null,
         _create: function () {
             var t = this;
             t._initElements();
@@ -8,6 +10,7 @@
         _initElements: function () {
             var t = this;
             t.formEl = t.element.parent();
+            t.buttonsEl = t.element.find('.buttons-container');
         },
         isSaved: false,
         _initEvents: function () {
@@ -44,6 +47,40 @@
                     loadingOverlayIsShowed = false;
                 }
             });
+
+            if (t.options.isFloatedButtons) {
+                setTimeout(function () {
+                    t.topOffset = null;
+                    t.initButtonsOffsetTop();
+                }, 1000);
+                t.initButtonsOffsetTop();
+                $(window).on('scroll', function () {
+                    t.positionizeButtons();
+                }).on('resize', function () {
+                    t.positionizeButtons();
+                });
+            }
+        },
+        initButtonsOffsetTop: function () {
+            let t = this;
+            if (t.topOffset === null) {
+                t.topOffset = t.buttonsEl.removeClass('floated-buttons').offset().top + t.buttonsEl.outerHeight(true);
+            }
+
+            t.positionizeButtons();
+        },
+        positionizeButtons: function () {
+            let t = this,
+                diff = $(window).scrollTop() + $(window).height() - t.topOffset + 5,
+                isFloat = diff < 0;
+            console.debug('Diff{' + diff + '}=$(window).scrollTop(){' + $(window).scrollTop() + '} + $(window).height(){' + $(window).height() + '} - t.topOffset{' + t.topOffset + '}');
+            if (isFloat) {
+                t.buttonsEl.addClass('floated-buttons');
+                let bottom = Math.floor(-diff - t.buttonsEl.outerHeight(true) + 5) + 'px';
+                t.buttonsEl.css('bottom', bottom);
+            } else {
+                t.buttonsEl.removeClass('floated-buttons');
+            }
         }
     })
 })();
